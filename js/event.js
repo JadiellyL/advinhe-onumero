377e017260e7d3c5b7724ece6c15ec98aec23f1c
JadiellyL <m.jadiellydlima@gmail.com>
2022-08-19 13:30:05 -0300

    Adicionando arquivos materialize e add toast no encerramento do jogo

--- js/event.js
+++ js/event.js
@@ -1,13 +1,14 @@
 let numeroAleatorio= Math.floor(Math.random() * 100) + 1;
 console.log(numeroAleatorio);
 let rodadas = 1;
+const elementsToast = document.querySelector("#btn-enviar");
 
 function jogo(){
     let palpite = Number(document.querySelector("#numero").value);
     let listaPalpites = document.getElementById("tentativas");
     let reultado = document.getElementById("certo-errado");
     let palpiteBaixo_ouAlto = document.getElementById("dica");
-
+    
 
     if(rodadas < 10){
         rodadas = rodadas + 1;
@@ -31,7 +32,19 @@ function jogo(){
     }else{
         palpite.disabled = true;
         reultado.innerHTML = "Jogo encerrado!"
-        alert(rodadas + " rodadas! fim de jogo");
+        //alert(rodadas + " rodadas! fim de jogo");
+        instanceToast();
     }
 
+}
+
+
+
+
+
+const instanceToast = ()=>{
+    M.toast({
+        html: "Jogo encerrado!",
+        classes: "rounded"
+    })
 }
